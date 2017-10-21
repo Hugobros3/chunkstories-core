0686ee518eb8f42249a8858857def35a742fcaf8
Gobrosse <hugo@xol.io>
2017-10-21 12:00:33 +0200

    Messing with the water texture

--- res/shaders/blocks_liquid_pass2/blocks_liquid_pass2.fs
+++ res/shaders/blocks_liquid_pass2/blocks_liquid_pass2.fs
@@ -55,28 +55,33 @@ uniform float underwater;
 
 vec3 mixedTextures(float blend, vec2 coords)
 {
-	return mix(texture(normalTextureShallow, coords).rgb, texture(normalTextureDeep, coords * 0.125).rgb, 0);
+	vec3 lel = texture(normalTextureDeep, coords * 0.125).rgb * 2.0 - vec3(1.0);
+	//lel = normalize(lel);
+	//return lel;
+	return mix(texture(normalTextureShallow, coords).rgb, texture(normalTextureDeep, coords * 0.125).rgb, 0) * 2.0 - vec3(1.0);
 }
 
 void main(){
 	vec3 normal = vec3(0.0, 0.0, 1.0);
 	
-	vec3 nt = 1.0*(mixedTextures(lightMapCoords.a, (vertexPassed.xz/5.0+vec2(0.0,time)/50.0)/15.0).rgb*2.0-1.0);
-	nt += 1.0*(mixedTextures(lightMapCoords.a, (vertexPassed.xz/2.0+vec2(-time,-2.0*time)/150.0)/2.0).rgb*2.0-1.0);
-	nt += 0.5*(mixedTextures(lightMapCoords.a, (vertexPassed.zx*0.8+vec2(400.0, sin(-time/5.0)+time/25.0)/350.0)/10.0).rgb*2.0-1.0);
-	nt += 0.25*(mixedTextures(lightMapCoords.a, (vertexPassed.zx*0.1+vec2(400.0, sin(-time/5.0)-time/25.0)/250.0)/15.0).rgb*2.0-1.0);
+	vec3 normalMap = 1.0 * vec3(0.0, 0.0, 1.0);
+	//normalMap += 0.5 * mixedTextures(lightMapCoords.a, (vertexPassed.xz/5.0+vec2(0.0,time)/50.0)/15.0);
+	normalMap += 0.8 * mixedTextures(lightMapCoords.a, (vertexPassed.xz/2.0+vec2(-time,-2.0*time)/150.0)/2.0);
+	normalMap *= mixedTextures(lightMapCoords.a, (vertexPassed.zx*0.8-vec2(400.0, 45.0 * sin(-time/5.0)+time/25.0)/350.0)/10.0) + 0.3 * mixedTextures(lightMapCoords.a, (vertexPassed.xz/16.0+vec2(-time,-3.0*time)/150.0)/2.0);
+	normalMap += 0.5 * mixedTextures(lightMapCoords.a, (vertexPassed.zx*0.8+vec2(400.0, sin(-time/5.0)+time/25.0)/350.0)/10.0);
+	normalMap += 0.25 * mixedTextures(lightMapCoords.a, (vertexPassed.zx*0.1+vec2(400.0, sin(-time/5.0)-time/25.0)/250.0)/15.0);
 	
-	nt = normalize(nt);
+	normalMap = normalize(normalMap);
 	
-	float i = 1.0;
+	//float i = 1.0;
 	
-	normal.x += nt.r*i;
-	normal.y += nt.g*i;
-	normal.z += nt.b*i;
+	//normal.x += nt.r*i;
+	//normal.y += nt.g*i;
+	//normal.z += nt.b*i;
 	
 	normal = normalize(normal);
 	
-	normal = perturb_normal(normalPassed, eyeDirection, texCoordPassed, normal);
+	normal = perturb_normal(normalPassed, eyeDirection, texCoordPassed, normalMap);
 	normal = normalize(normalMatrix * normal);
 	
 	//Basic texture color
