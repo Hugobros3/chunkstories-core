69447c3674450e5457377b8b76a0327171d6d701
Gobrosse <hugo@xol.io>
2019-09-19 19:40:32 +0200

    simplex stuff
    ffs
    
    duh
    
    remove useless test
    
    remove test

--- res/shaders/blockMeshes/blockMeshes.vert
+++ res/shaders/blockMeshes/blockMeshes.vert
@@ -19,17 +19,56 @@ uniform Camera camera;
 //uniform ChunkRenderInfo chunkInfo;
 instanced ChunkRenderInfo chunkInfo; // look mom, no uniforms !
 
+#define PI 3.14159265359
+#define pi PI
+#include struct xyz.chunkstories.api.math.random.PrecomputedSimplexSeed
+uniform PrecomputedSimplexSeed simplexSeed;
+
+#include simplex.glsl
+#include noise.glsl
+
+float heightAt(vec2 pos) {
+	float height = 0.0;
+
+	float baseHeight = ridgedNoise(pos, 5, 1.0, 0.5);
+	height += baseHeight * 64.0;
+
+	float mountainFactor = fractalNoise(pos + vec2(548.0, 330.0), 3, 0.5, 0.5);
+	mountainFactor *= (1.0 + 0.125 * ridgedNoise(pos + vec2(14, 9977), 2, 4.0, 0.7));
+	mountainFactor -= 0.3;
+	mountainFactor /= (1.0 - 0.3);
+	mountainFactor = clamp(mountainFactor, 0.0, 1.0);
+
+	height += mountainFactor * 128;
+
+	float plateaHeight = clamp(fractalNoise(pos+vec2(225.0, 321.0), 3, 1.0, 0.5) * 32.0 - 8.0, 0.0, 1.0);
+	plateaHeight *= clamp(fractalNoise(pos+vec2(3158.0, 9711.0), 3, 0.125, 0.5) * 0.5 + 0.5, 0.0, 1.0);
+
+	if(height > 48)
+		height += plateaHeight * 24.0;
+	else
+		height += plateaHeight * baseHeight * 24.0;
+
+	return height;
+}
+
 void main()
 {
 	vec3 vertexPos = vertexIn.xyz;
 	vertexPos += vec3(chunkInfo.chunkX, chunkInfo.chunkY, chunkInfo.chunkZ) * 32.0;
 
+	//if((mod(vertexPos.x , 32.0) < 16.0) == (mod(vertexPos.z , 32.0) < 16.0))
+	//	vertexPos.y = 1+floor(heightAt(vec2(floor(vertexPos.x), floor(vertexPos.z))));
+
 	//vec4 viewSpace = camera.viewMatrix * vec4(vertexPos, 1.0);
 	//vec4 projected = camera.projectionMatrix * viewSpace;
 	vec4 projected = camera.combinedViewProjectionMatrix * vec4(vertexPos, 1.0);
 
+
 	vertex = vertexPos;
+
 	color = vec4(colorIn.x, colorIn.y, colorIn.z, 1.0);
+	//color = vec4(1.0);
 	normal = camera.normalMatrix * normalIn;
 	texCoord = texCoordIn;
 	textureId = int(textureIdIn);
--- src/main/java/xyz/chunkstories/core/generator/HorizonGenerator.kt
+++ src/main/java/xyz/chunkstories/core/generator/HorizonGenerator.kt
@@ -29,10 +29,10 @@ open class HorizonGenerator(definition: WorldGeneratorDefinition, world: World) 
     private val worldSizeInBlocks = world.sizeInChunks * 32
 
     private val waterHeight = definition["waterHeight"].asInt ?: 48
-    private val mountainScale = definition["mountainScale"].asDouble ?: 0.3
+    private val mountainScale = definition["mountainScale"].asDouble ?: 128.0
     private val mountainOffset = definition["mountainOffset"].asDouble ?: 0.3
     private val baseHeightScale = definition["baseHeightScale"].asInt ?: 64
-    private val plateauHeightScale = definition["plateauHeightScale"].asInt ?: 64
+    private val plateauHeightScale = definition["plateauHeightScale"].asInt ?: 24
 
     private val airVoxel: Voxel = world.gameContext.content.voxels.air
     private val stoneVoxel: Voxel = world.gameContext.content.voxels.getVoxel("stone")!!
@@ -239,14 +239,14 @@ open class HorizonGenerator(definition: WorldGeneratorDefinition, world: World) 
     }
 
     private fun fractalNoise(x: Int, z: Int, octaves: Int, freq: Float, persistence: Float): Float {
-        var freq = freq
+        var frequency = freq
         var total = 0.0f
         var maxAmplitude = 0.0f
         var amplitude = 1.0f
-        freq *= (worldSizeInBlocks / (64 * 32)).toFloat()
+        frequency *= (worldSizeInBlocks / (64 * 32)).toFloat()
         for (i in 0 until octaves) {
-            total += ssng.looped_noise(x * freq, z * freq, worldSizeInBlocks.toFloat()) * amplitude
-            freq *= 2.0f
+            total += ssng.looped_noise(x * frequency, z * frequency, worldSizeInBlocks.toFloat()) * amplitude
+            frequency *= 2.0f
             maxAmplitude += amplitude
             amplitude *= persistence
         }
@@ -254,14 +254,15 @@ open class HorizonGenerator(definition: WorldGeneratorDefinition, world: World) 
     }
 
     private fun ridgedNoise(x: Int, z: Int, octaves: Int, freq: Float, persistence: Float): Float {
-        var freq = freq
+        var frequency = freq
         var total = 0.0f
         var maxAmplitude = 0.0f
         var amplitude = 1.0f
-        freq *= (worldSizeInBlocks / (64 * 32)).toFloat()
+        frequency *= (worldSizeInBlocks / (64 * 32)).toFloat()
+
         for (i in 0 until octaves) {
-            total += (1.0f - abs(ssng.looped_noise(x * freq, z * freq, worldSizeInBlocks.toFloat()))) * amplitude
-            freq *= 2.0f
+            total += (1.0f - abs(ssng.looped_noise(x * frequency, z * frequency, worldSizeInBlocks.toFloat()))) * amplitude
+            frequency *= 2.0f
             maxAmplitude += amplitude
             amplitude *= persistence
         }
@@ -272,7 +273,6 @@ open class HorizonGenerator(definition: WorldGeneratorDefinition, world: World) 
         var height = 0.0
 
         val baseHeight = ridgedNoise(x, z, 5, 1.0f, 0.5f)
-
         height += baseHeight * baseHeightScale
 
         var mountainFactor = fractalNoise(x + 548, z + 330, 3, 0.5f, 0.5f)
@@ -309,4 +309,5 @@ open class HorizonGenerator(definition: WorldGeneratorDefinition, world: World) 
 
         return Math2.clamp(f.toDouble(), 0.0, 1.0)
     }*/
+
 }
